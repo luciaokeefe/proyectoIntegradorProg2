21d94acb99871e9148b9f14d68d564e228c7c40b
marinaberaud <mberaud@udesa.edu.ar>
2022-04-05 19:48:43 -0300

    terminamos con los datos de anteojos (estamos las 3)

--- db/anteojos.js
+++ db/anteojos.js
@@ -1,5 +1,3 @@
-const { BandwidthLimitExceeded } = require("http-errors");
-
 const anteojos = [
     {
         marca: "Ray Ban",
@@ -7,17 +5,16 @@ const anteojos = [
         colorDelArmazon: "Carey",
         colorDelLente: "Marrón degradé",
         id: 1,
-        //img: 
-
+        img: "/images/products/id1.png", //supuestamente esta bien
     },
 
     {
-        marca:  "Ray Ban",
+        marca: "Ray Ban",
         modelo: "Scuderia Ferrari Collection",
         colorDelArmazon: "Petróleo",
         colorDelLente: "Celeste",
         id: 2,
-        //img:
+        img: "/images/products/id2.png",
     },
 
     {
@@ -25,9 +22,73 @@ const anteojos = [
         modelo: "Catwalk",
         colorDelArmazon: "Negro",
         colorDelLente: "Gris",
-        id: 3
-        //img:
+        id: 3,
+        img: "/images/products/id1.png"
     },
 
-    
+    {
+        marca: "Gucci",
+        modelo: "Marine",
+        colorDelArmazon: "Varios",
+        colorDelLente: "violeta ",
+        id: 4,
+        img: "/images/products/id4.png"
+    },
+
+    {
+        marca: "Christian Dior Paris",
+        modelo: "Caleya",
+        colorDelArmazon: "Azul y dorado",
+        colorDelLente: "Azul",
+        id: 5,
+        img: "/images/products/id5.png",
+    },
+
+    {
+        marca: "Gucci",
+        modelo: "Lulú",
+        colorDelArmazon: "Carey",
+        colorDelLente: "Celeste",
+        id: 6,
+        img: "/images/products/id6.png",
+    },
+
+    {
+        marca: "Christian Dior Paris",
+        modelo: "Moana",
+        colorDelArmazon: "Gris y dorado",
+        colorDelLente: "Gris degradé",
+        id: 7,
+        img: "/images/products/id7.png"
+    },
+
+
+    {
+        marca: "Christian Dior Paris",
+        modelo: "Le carré",
+        colorDelArmazon: "Dorado",
+        colorDelLente: "Azul",
+        id: 8,
+        img: "/images/products/id8.png"
+    },
+
+    {
+        marca: "Balenciaga",
+        modelo: "Snowboard",
+        colorDelArmazon: "Blanco",
+        colorDelLente: "Celeste",
+        id: 9,
+        img: "/images/products/id9.png",
+    },
+
+    {
+        marca: "Gentle Monster",
+        modelo: "California Sunset",
+        colorDelArmazon: "Negro",
+        colorDelLente: "Naranja",
+        id: 10,
+        img: "/images/products/id1.png"
+    },
+
+
 ]
