684f1f32b7ede4ad8d7b8805d4d7b863f5255b3b
calawalker <cwalker@udesa.edu.ar>
2022-04-13 17:53:33 -0300

    controllers, rutas, footer y parte de la tipografia

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var controllers = require("../controllers/users")
 
 router.get('/', controllers.users);
-// router.get('/edit', controllers.users);
 
 router.get('/me', controllers.users);
 router.get('/me/edit', controllers.edit);
