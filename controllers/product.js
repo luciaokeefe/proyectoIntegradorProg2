4bf84161d7baef04819156fc3b111a3c88021a21
calawalker <cwalker@udesa.edu.ar>
2022-04-15 15:35:27 -0300

    ultimos detalles y resolución de conflictos

--- controllers/product.js
+++ controllers/product.js
@@ -1,13 +1,8 @@
 var data = require('../db/data');
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 34c37c5907f5176a639fbe48b466d12f166476db
 
 const controllers = {
     show: function (req, res) {
-        res.render('product', {product: data[req.params.id -1], user: data.users, comment: data.comments})
+        res.render('product', {product: data.products[req.params.id -1], user: data.users, comment: data.comments})
     },
     add: function (req, res) {
         res.render('productAdd') 
