2198e3d8915e4f190f27f4ed4d4e471f5a49b21b
calawalker <cwalker@udesa.edu.ar>
2022-05-31 11:55:01 -0300

    avanzamos con register

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
-const { send } = require("express/lib/response");
 var db = require("../database/models");
-
+var hasher = require("bcryptjs");
 
 const controllers = {
     indexAll: function (req, res) {
@@ -20,6 +19,23 @@ const controllers = {
     register: function (req, res) {
         res.render('register');
     },
+
+    store: function(req, res) {
+        if (!req.body.email) { throw Error('Not email provided.') }
+        const hashedPassword = hasher.hashSync(req.body.password, 10);
+        db.User.create({
+                username: req.body.username,
+                password: hashedPassword,
+                email: req.body.email,
+            })
+            .then(function () {
+                res.redirect('/');
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+    },
+
     results: function (req, res) {
         res.render('searchResults');
 
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,14 @@ var controllers = require('../controllers/index');
 
 
 router.get('/', controllers.indexAll);
+
 router.get('/login', controllers.login);
+
 router.get('/register', controllers.register);
+router.post('/register', controllers.store);
+
 router.get('/searchResults', controllers.results);
+
+
 
 module.exports = router;
