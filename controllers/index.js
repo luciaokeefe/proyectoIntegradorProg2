969f72f0cdf14ebcfc8a56953918022b4f859623
marinaberaud <mberaud@udesa.edu.ar>
2022-06-13 17:51:05 -0300

    avanzamos diferentes temas (estamos las tres)

--- controllers/index.js
+++ controllers/index.js
@@ -1,25 +1,29 @@
 var db = require("../database/models");
 var hasher = require("bcryptjs");
+var op = db.Sequelize.Op;
+
 
 const controllers = {
     indexAll: function (req, res) {
-        db.Product.findAll()
-            .then(function (products) {
-                res.render ('index', { products });
-            })    
-            .catch(function (error) {
-                    res.send(error)
-                })
+        db.Product.findAll({
+            order: [['id', 'DESC']],
 
+        } )
+            .then(function (products) {
+                res.render('index', { products });
+            })
+            .catch(function (error) {
+                res.send(error)
+            })
     },
 
     login: function (req, res) {
         res.render('login');
     },
 
-    access: function(req, res, next) {
-        db.User.findOne({ where: { email: req.body.mail }})
-            .then(function(user) {
+    access: function (req, res, next) {
+        db.User.findOne({ where: { email: req.body.mail } })
+            .then(function (user) {
                 if (!user) throw Error('User not found.')
                 if (hasher.compareSync(req.body.password, user.Password)) {
                     req.session.user = user;
@@ -46,34 +50,34 @@ const controllers = {
         res.render('register');
     },
 
-    store: function(req, res) {
-        try{
-            if(!req.body.username) {throw Error ('Campo de nombre de usuario vacío.') }
-            if(!req.body.email) {throw Error ('Campo de email vacío.') }
-            if(req.body.Password.length < 3) {throw Error ('Contraseña muy corta.') }
-            if(!req.body.Password) {throw Error ('Campo de contraseña vacío') }
-            if(!req.body.name) {throw Error ('Campo de nombre completo vacío') }
-            if(!req.body.surname) {throw Error ('Campo de apellido vacío') }
-            if(!req.body.DNI) {throw Error ('Campo de DNI vacío') }
+    store: function (req, res) {
+        try {
+            if (!req.body.username) { throw Error('Campo de nombre de usuario vacío.') }
+            if (!req.body.email) { throw Error('Campo de email vacío.') }
+            if (req.body.Password.length < 3) { throw Error('Contraseña muy corta.') }
+            if (!req.body.Password) { throw Error('Campo de contraseña vacío') }
+            if (!req.body.name) { throw Error('Campo de nombre completo vacío') }
+            if (!req.body.surname) { throw Error('Campo de apellido vacío') }
+            if (!req.body.DNI) { throw Error('Campo de DNI vacío') }
 
-        }catch(error){
-            res.render('register', {error: error.message});
+        } catch (error) {
+            res.render('register', { error: error.message });
             return;
         }
         if (!req.body.email) { throw Error('Not email provided.') }
         const hashedPassword = hasher.hashSync(req.body.Password, 10);
         db.User.create({
-                name: req.body.name,
-                surname: req.body.surname,
-                username: req.body.username,
-                email: req.body.email,
-                phone: req.body.phone,
-                birthdate: req.body.birthdate,
-                gender: req.body.gender,
-                DNI: req.body.DNI,
-                profilephoto: req.body.profilephoto,
-                Password: hashedPassword,    
-            })
+            name: req.body.name,
+            surname: req.body.surname,
+            username: req.body.username,
+            email: req.body.email,
+            phone: req.body.phone,
+            birthdate: req.body.birthdate,
+            gender: req.body.gender,
+            DNI: req.body.DNI,
+            profilephoto: req.body.profilephoto,
+            Password: hashedPassword,
+        })
             .then(function () {
                 res.redirect('/');
             })
@@ -82,11 +86,28 @@ const controllers = {
             })
     },
 
-    results: function (req, res) {
-        res.render('searchResults');
+    results: function (req, res, next) {
+        db.Product.findAll({
+            where: {
+                [op.or]: [
+                    { modelo: { [op.like]: "%" + req.query.criteria + "%" } },
+                    { marca: { [op.like]: "%" + req.query.criteria + "%" } }
+                ]
+            },
+        })
 
-    }
+            .then(function (product) {
+                if (product.length < 1) { throw Error('No hay resultados para su búsqueda') }
+                else {
+                    res.render('searchResults', { product });
+                }
+            })
+            .catch(function (error) {
+                res.render('searchResults', { product:[], error });
+            });
+    },
 }
 
 module.exports = controllers;
+
 
