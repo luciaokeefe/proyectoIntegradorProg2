4d21a4293f8a27863fc038637110adcc3ce4bea8
marinaberaud <mberaud@udesa.edu.ar>
2022-06-18 17:36:03 -0300

    seguimos con la aparición del creador en el detalle de productos y más (estamos las 3)

--- controllers/product.js
+++ controllers/product.js
@@ -2,84 +2,91 @@ var db = require('../database/models');
 
 const controllers = {
     show: function (req, res) {
-    db.Product.findByPk(req.params.id, { include:  { all: true, nested: true}})
-        .then(function (product) {
-            res.render('product', {product}) 
-        })
-        .catch(function (error) {
-            res.send(error)
-        })
+        db.Product.findByPk(req.params.id, { include: { all: true, nested: true } })
+            .then(function (product) {
+                res.render('product', { product })
+            })
+            .catch(function (error) {
+                res.send(error)
+            })
     },
-    add: function(req, res) {
-        if (!req.session.user) { 
+    add: function (req, res) {
+        if (!req.session.user) {
             throw Error('Not authorized.')
         }
         res.render('productAdd');
     },
-    edit: function(req, res) {
-        db.Product.findByPk(req.params.id)
+    edit: function (req, res) {
+        if (req.body.user_id !== req.session.user) {
+            throw Error('Not authorized.');
+        }
+        else {
+            db.Product.findByPk(req.params.id)
+                .then(function (product) {
+                    res.render('productEdit', { product });
+                })
+                .catch(function (error) {
+                    res.send(error);
+                })
+        } 
+        
+    },
+    update: function (req, res) {
+        if (req.file) req.body.IMG = (req.file.path).replace('public', '');
+        db.Product.update(req.body, { where: { id: req.params.id } })
             .then(function (product) {
-                res.render('productEdit', { product });
+                res.redirect('/product/' + req.params.id)
             })
             .catch(function (error) {
                 res.send(error);
             })
     },
-    update: function(req, res) {
-        if (req.file) req.body.IMG = (req.file.path).replace('public', '');
-        db.Product.update(req.body, { where: { id: req.params.id } })
-            .then(function(product) {
-                res.redirect('/product/' + req.params.id)
-            })
-            .catch(function(error) {
-                res.send(error);
-            })
-    },
-    store: function(req, res) {
-        if (!req.session.user) { 
+    store: function (req, res) {
+        if (!req.session.user) {
             return res.render('productAdd', { error: 'Not authorized.' });
         }
         req.body.user_id = req.session.user.id;
         if (req.file) req.body.IMG = (req.file.path).replace('public', '');
         db.Product.create(req.body)
-            .then(function() {
+            .then(function () {
                 res.redirect('/')
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 res.send(error);
             })
     },
 
-    comment: function(req, res) {
+    comment: function (req, res) {
 
-        if (!req.session.user) { 
+        if (!req.session.user) {
             return res.redirect('/login/');
         }
-       else {
-        // Set user from session user
-        req.body.user_id = req.session.user.id;
-        
-        // Set product from url params
-        req.body.product_id = req.params.id;
-        
-        db.Comment.create(req.body)        
-            .then(function() {
-                res.redirect('/product/' + req.params.id)
-            })
-            .catch(function(error) {
-                res.send(error);
-            })      
-    }},
+        else {
+            // Set user from session user
+            req.body.user_id = req.session.user.id;
 
-    delete: function(req, res) {
+            // Set product from url params
+            req.body.product_id = req.params.id;
+
+            db.Comment.create(req.body)
+                .then(function () {
+                    res.redirect('/product/' + req.params.id)
+                })
+                .catch(function (error) {
+                    res.send(error);
+                })
+        }
+    },
+
+    delete: function (req, res) {
         if (!req.session.user) {
             throw Error('Not authorized.')
         }
         db.Product.destroy({ where: { id: req.params.id } })
-            .then(function() {
+            .then(function () {
                 res.redirect('/')
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 res.send(error);
             })
     }
